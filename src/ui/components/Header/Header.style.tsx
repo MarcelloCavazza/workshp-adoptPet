f3286d1a676bf994b85981962ec2ba2e16133f82
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2022-06-02 22:44:52 -0300

    consertando erro de path

--- src/ui/components/Header/Header.style.tsx
+++ src/ui/components/Header/Header.style.tsx
@@ -1,15 +1,15 @@
-import { styled } from "@mui/system";
+import { styled } from "@mui/material";
 
 
 export const ContainerHeader = styled('header')`
     display: flex;
     justify-content:center;
     border-bottom: 1px solid #F0F0F0;
-    padding: ${({theme})=> theme.spacing()}
+    padding: ${({ theme }) => theme.spacing()}
     `
-;
+    ;
 
 export const Logo = styled('img')`
     width: 190px;
     `
-;
+    ;
